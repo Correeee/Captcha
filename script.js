33531f168b3d5b5fd89b0c9f3224c4e26ba1ae3f
elcorre <correa755@hotmail.com>
2023-02-02 16:22:53 -0300

    personaje

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let sonido_correcto = new Audio("multimedia/notification.wav")
-
 let sonido_error = new Audio("multimedia/error.mp3")
 
 let caracteres = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 let captcha_numerico = []
 let captcha = []
 
-let html_captcha = document.getElementById("palabra_captcha")
 
+let html_captcha = document.getElementById("palabra_captcha")
 
 function caracter_al_azar(cantidad){
     for (let i = 0; i < cantidad; i++) { //CREA ${CANTIDAD} DE NUMEROS AL AZAR Y LOS PUSHEA A UN ARRAY
@@ -24,7 +23,7 @@ function caracter_al_azar(cantidad){
         captcha.push(caracteres[numero])
     }
     captcha = captcha.join("") //CONVIERTE EL ARRAY EN STRING Y UNE LOS CARACTERES
-    console.log(captcha)
+    //console.log(captcha)
     html_captcha.innerText = `${captcha}`
 
 }
@@ -36,18 +35,18 @@ let input_captcha = document.getElementById("input_captcha")
 
 input_captcha.addEventListener("keyup" , function(e){
     let respuesta = e.target.value;
-    console.log(respuesta)
+    //console.log(respuesta)
 
     if(respuesta == captcha){
         sonido_correcto.play()
-        html_captcha.innerHTML = `¡Eres humano<img src="./multimedia/./mario.gif" alt="" class="mario">!`
+        html_captcha.innerHTML = `¡Eres humano!<img src="./multimedia/./mario.gif" alt="" class="personaje">`
         html_captcha.style.backgroundColor="#52b788"
         html_captcha.style.animation="shake 2s linear infinite"
     }
     if(respuesta.length > 4){
         if(respuesta != captcha){
             sonido_error.play()
-            html_captcha.innerText = `¡Maldito Robot!`
+            html_captcha.innerHTML = `¡Maldito Robot!<img src="./multimedia/./robot.gif" alt="" class="personaje">`
             html_captcha.style.backgroundColor="red"
             setTimeout(recargar , 2000)
         }
